d35c0cf4cb483d170cbff3f14694d552f1418226
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-02-24 16:23:45 +0200

    Initial commit

---
+++ unimanager/src/main/java/com/theconquerors/unimanager/UnimanagerApplication.java
@@ -0,0 +1,13 @@
+package com.theconquerors.unimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimanagerApplication.class, args);
+	}
+
+}
---
+++ unimanager/src/test/java/com/theconquerors/unimanager/UnimanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theconquerors.unimanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
