067b7086581222c37c4a5a6a3dd721a07be60a86
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-02-26 15:34:28 +0200

    Add Semester class

---
+++ unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Semester.java
@@ -0,0 +1,31 @@
+package com.theconquerors.unimanager.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "semesters")
+public class Semester extends BaseEntity {
+
+    @NotNull
+    @Column(name = "number", nullable = false)
+    private int number;
+
+    @NotNull
+    @Column(name = "startDate", nullable = false)
+    private Date startDate;
+
+    @NotNull
+    @Column(name = "endDate", nullable = false)
+    private Date endDate;
+}
--- unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Specialty.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Specialty.java
@@ -1,11 +1,19 @@
 package com.theconquerors.unimanager.model.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "specialties")
 public class Specialty {
 
     @NotBlank
