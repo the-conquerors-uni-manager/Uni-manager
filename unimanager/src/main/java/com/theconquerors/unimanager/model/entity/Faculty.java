830c5939f03dddf23bf353c186c759135e932968
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-02-27 14:05:06 +0200

    Update Faculty

--- unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Faculty.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Faculty.java
@@ -1,8 +1,6 @@
 package com.theconquerors.unimanager.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -17,9 +15,10 @@ import java.util.List;
 public class Faculty extends BaseEntity{
 
     @NotBlank
-    @Column(name = "faculty", nullable = false)
+    @Column(name = "facultyName", nullable = false)
     @Size(max = 250)
-    private String faculty;
+    private String facultyName;
 
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL)
     private List<Specialty> specialties;
 }
