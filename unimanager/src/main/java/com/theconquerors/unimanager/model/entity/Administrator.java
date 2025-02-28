0851e3242e279375e37c09d6943a2635e9d8798a
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-02-28 17:07:10 +0200

    Update Administrator

--- unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Administrator.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Administrator.java
@@ -1,8 +1,6 @@
 package com.theconquerors.unimanager.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
