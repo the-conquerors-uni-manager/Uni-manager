15efe58549747a1d90246bc7471221b734b83193
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-03-04 22:08:36 +0200

    Update Specialty

--- unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Specialty.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/model/entity/Specialty.java
@@ -1,5 +1,6 @@
 package com.theconquerors.unimanager.model.entity;
 
+import com.theconquerors.unimanager.model.entity.enums.StudyStageEnum;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,6 +23,10 @@ public class Specialty extends BaseEntity {
     @NotBlank
     @Column(name = "description", nullable = true,columnDefinition = "TEXT")
     private String description;
+
+    @NotBlank
+    @Column(name = "studyStage", nullable = false)
+    private StudyStageEnum studyStage;
 
     @ManyToOne(fetch = FetchType.LAZY, targetEntity =  Faculty.class)
     @JoinColumn(name = "faculty_id", referencedColumnName = "id",nullable = false)
