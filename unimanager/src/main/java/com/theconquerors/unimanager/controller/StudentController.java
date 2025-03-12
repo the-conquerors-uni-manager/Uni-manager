de431cacb331ae28bc971a12e05bbeea3f467220
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-03-12 10:24:45 +0200

    Add StudentController

--- unimanager/src/main/java/com/theconquerors/unimanager/controller/StudentController.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/controller/StudentController.java
@@ -1,4 +1,40 @@
 package com.theconquerors.unimanager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+
+    private static final Logger log = LoggerFactory.getLogger(StudentController.class);
+
+    @GetMapping("")
+    public String login(Model model){
+
+        return "redirect:login";
+    }
+    @GetMapping("/{studentId}")
+    public String information(@PathVariable("studentId") String studentId,Model model){
+
+        return "";
+    }
+
+    @GetMapping("/grades/{studentId}")
+    public String grades(@PathVariable("studentId") String studentId, Model model){
+
+        return "";
+    }
+
+    @GetMapping("/weekly-schedule/{studentId}")
+    public String weeklySchedule(@PathVariable("studentId") String studentId,Model model){
+
+        return "";
+    }
+
 }
