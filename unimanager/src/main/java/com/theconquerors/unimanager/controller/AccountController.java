b057ec2fb435b1f46a4937b6a5c9585b3c40ee7c
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-03-14 16:37:54 +0200

    Update AccountController
    redirect to login page

--- unimanager/src/main/java/com/theconquerors/unimanager/controller/AccountController.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/controller/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
-public class AcountControler {
+public class AccountController {
     @GetMapping("")
     public String goToLogin(Model model){
         return "redirect:login";
