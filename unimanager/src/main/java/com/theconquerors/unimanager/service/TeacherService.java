b72cddb09896fce3cdc10d97edcb37b3a5401203
KristianTV <75727323+KristianTV@users.noreply.github.com>
2025-03-14 16:41:55 +0200

    Add Teacher Service
    basic skeleton

--- unimanager/src/main/java/com/theconquerors/unimanager/service/TeacherService.java
+++ unimanager/src/main/java/com/theconquerors/unimanager/service/TeacherService.java
@@ -4,9 +4,11 @@ import com.theconquerors.unimanager.model.entity.*;
 import com.theconquerors.unimanager.repository.GradeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TeacherService {
 
     private static final Logger log = LoggerFactory.getLogger(TeacherService.class);
